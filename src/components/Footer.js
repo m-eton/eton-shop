19ff72e7e87410227042a05d26eff12a6615bdae
milos <obrovac.milos@gmail.com>
2020-01-17 11:36:06 +0100

    footer done

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="footer">
+    <p>
+      created by{" "}
+      <a
+        href="https://milos.netlify.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        @milos_dev
+      </a>
+    </p>
+  </footer>
+);
+
+export default Footer;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import ShopPage from "../components/ShopPage";
 import CartPage from "../components/CartPage";
 import NotFoundPage from "../components/NotFoundPage";
@@ -16,6 +17,7 @@ const AppRouter = () => (
         <Route component={NotFoundPage} />
       </Switch>
     </div>
+    <Footer />
   </BrowserRouter>
 );
 
