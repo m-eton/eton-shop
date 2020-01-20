516275e2826323293b9401b72a47f9941c4d143c
milos <obrovac.milos@gmail.com>
2020-01-20 09:07:46 +0100

    confirm removal of an item in cart

--- src/components/CartPageItem.js
+++ src/components/CartPageItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import {
   FaRegPlusSquare,
@@ -6,51 +6,78 @@ import {
   FaWindowClose
 } from "react-icons/fa";
 
-const CartPageItem = props => {
-  const { id, title, price, image } = props.product;
-  const { counter, counterPlus, counterMinus, removeFromCart } = props;
-  return (
-    <>
-      <div className="cart-item-card" key={id}>
-        <div className="cart-item-img">
-          <img src={image} alt={title + id} />
-        </div>
+export default class CartPageItem extends Component {
+  state = {
+    removalQuestion: false
+  };
 
-        <div className="cart-item-label">
-          <div className="cart-item-row">
-            <div className="title">{title}</div>
-            <div
-              className="cart-item-control close"
-              onClick={() => removeFromCart(id)}
-            >
-              <FaWindowClose />
-            </div>
+  confirmRemoval = () => {
+    this.setState(() => ({ removalQuestion: true }));
+  };
+  confirmRemovalNo = () => {
+    this.setState(() => ({ removalQuestion: false }));
+  };
+
+  render() {
+    const { id, title, price, image } = this.props.product;
+    const { counter, counterPlus, counterMinus, removeFromCart } = this.props;
+    return (
+      <>
+        <div className="cart-item-card" key={id}>
+          <div className="cart-item-img">
+            <img src={image} alt={title + id} />
           </div>
-          <div className="cart-item-row">
-            <div className="cart-item-price">
-              <div className="price">{price}$</div>
-              <div className="count">{counter > 1 && " x " + counter}</div>
-            </div>
-            <div className="cart-item-controls">
+
+          <div className="cart-item-label">
+            <div className="cart-item-row">
+              <div className="title">{title}</div>
               <div
-                className="cart-item-control"
-                onClick={() => counterMinus(id)}
+                className="cart-item-control close"
+                onClick={() => this.confirmRemoval()}
               >
-                <FaRegMinusSquare />
-              </div>
-              <div
-                className="cart-item-control"
-                onClick={() => counterPlus(id)}
-              >
-                <FaRegPlusSquare />
+                <FaWindowClose />
               </div>
             </div>
+            <div className="cart-item-row">
+              <div className="cart-item-price">
+                <div className="price">{price}$</div>
+                <div className="count">{counter > 1 && " x " + counter}</div>
+              </div>
+              <div className="cart-item-controls">
+                <div
+                  className="cart-item-control"
+                  onClick={() => counterMinus(id)}
+                >
+                  <FaRegMinusSquare />
+                </div>
+                <div
+                  className="cart-item-control"
+                  onClick={() => counterPlus(id)}
+                >
+                  <FaRegPlusSquare />
+                </div>
+              </div>
+            </div>
+            {this.state.removalQuestion && (
+              <div className="cart-page-item-removal-confirmation">
+                <p className="removal-confirmation-text">
+                  Remove this item form cart?
+                </p>
+                <p className="answer" onClick={() => removeFromCart(id)}>
+                  yes
+                </p>
+                <p
+                  className="answer no"
+                  onClick={() => this.confirmRemovalNo()}
+                >
+                  no
+                </p>
+              </div>
+            )}
           </div>
         </div>
-      </div>
-      <hr />
-    </>
-  );
-};
-
-export default CartPageItem;
+        <hr />
+      </>
+    );
+  }
+}
